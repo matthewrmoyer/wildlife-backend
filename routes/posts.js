3aa402faad792f8cd7705aa09abdc9706cd78c27
Matthew Moyer <matthewrmoyer@gmail.com>
2017-05-26 14:26:50 -0600

    add post route

--- routes/posts.js
+++ routes/posts.js
@@ -9,4 +9,17 @@ router.get('/', (req, res) => {
 		})
 })
 
+router.post('/', (req, res) => {
+	knex('post')
+		.insert({
+			user_email: req.body.email,
+			user_name: req.body.name,
+			latitude: req.body.latitude,
+			longitude: req.body.longitude,
+			specie: req.body.specie,
+			description: req.body.description,
+			image_url: req.body.image_url
+		})
+})
+
 module.exports = router;
