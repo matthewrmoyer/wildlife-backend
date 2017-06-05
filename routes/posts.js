0434135d7eabc4377adb240c805eead1bea09381
Matthew Moyer <matthewrmoyer@gmail.com>
2017-06-05 13:43:46 -0600

    messing with image

--- routes/posts.js
+++ routes/posts.js
@@ -2,11 +2,15 @@ const express = require('express')
 const router = express.Router()
 const knex = require('../db/knex.js')
 
-const upload  = require('multer')();
+const upload = require('multer')();
 const AWS = require('aws-sdk');
 const uuid = require('uuid/v4');
 
-AWS.config.update({ accessKeyId: process.env.S3_KEY, secretAccessKey: process.env.S3_SECRET });
+AWS.config.update({
+	accessKeyId: process.env.S3_KEY,
+	secretAccessKey: process.env.S3_SECRET
+});
+
 const s3 = new AWS.S3();
 
 
@@ -36,28 +40,32 @@ router.post('/', (req, res) => {
 
 
 router.get('/image', (req, res, next) => {
-  s3.listObjects({Bucket: process.env.S3_BUCKET}, (err, resp) => {
-    if (err) {
-      next(err)
-    } else {
-      res.json({resp})
-    }
-  })
+	s3.listObjects({
+		Bucket: process.env.S3_BUCKET
+	}, (err, resp) => {
+		if (err) {
+			next(err)
+		} else {
+			res.json({
+				resp
+			})
+		}
+	})
 })
 
 router.post('/image', upload.single('image'), (req, res) => {
-  let id = uuid();
-  s3.putObject({
-    Bucket: process.env.S3_BUCKET,
-    Key: id,
-    Body: new Buffer(req.file.buffer)
-  }, err => {
-    if (err) {
-      next(err)
-    } else {
-      res.json(`{"success": true}`)
-    }
-  });
+	let id = uuid();
+	s3.putObject({
+		Bucket: process.env.S3_BUCKET,
+		Key: id,
+		Body: new Buffer(req.file.buffer)
+	}, err => {
+		if (err) {
+			console.log(err)
+		} else {
+			res.json(`{"success": true}`)
+		}
+	});
 })
 
 // {
